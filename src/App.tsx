85ef5f402dd28f4bd454e27ea4fc9c8382bdce4f
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-22 20:02:16 +0530

    Barchart file has been created with style file and called in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import BarChart from './Components/BarChart/BarChart';
 import ScatterPlot from './Components/ScatterPlot/ScatterPlot';
 
 function App() {
   return (
     <div className='App'>
       <ScatterPlot />
+      <BarChart />
     </div>
   );
 }
---
+++ src/Components/BarChart/BarChart.tsx
@@ -0,0 +1,11 @@
+import styles from './BarChart.module.css';
+
+const BarChart = () => {
+  return (
+    <div>
+      <h1>BarChart</h1>
+    </div>
+  );
+};
+
+export default BarChart;
