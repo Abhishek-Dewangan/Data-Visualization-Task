d7b20d892be441343a9ebf5a626dca67bc3999fb
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-22 20:00:21 +0530

    Scatterplot file has been created with style file and called in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import ScatterPlot from './Components/ScatterPlot/ScatterPlot';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <ScatterPlot />
     </div>
   );
 }
---
+++ src/Components/ScatterPlot/ScatterPlot.tsx
@@ -0,0 +1,11 @@
+import styles from './ScatterPlot.module.css';
+
+const ScatterPlot = () => {
+  return (
+    <div>
+      <h1>ScatterPlot</h1>
+    </div>
+  );
+};
+
+export default ScatterPlot;
