7c58d5733dbc18e3631d2615aec8a2d95ab1fb67
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-02-22 20:51:33 +0530

    Scatter plot between color intensity and hue has been plotted

--- src/Components/ScatterPlot/ScatterPlot.tsx
+++ src/Components/ScatterPlot/ScatterPlot.tsx
@@ -1,11 +1,18 @@
 import styles from './ScatterPlot.module.css';
-import {scatterData} from '../../Assets/WineData'
+import {scatterData} from '../../Assets/WineData';
+import ReactEcharts from 'echarts-for-react';
 
 const ScatterPlot = () => {
-    console.log(scatterData)
+  console.log(scatterData);
+  const option = {
+    xAxis: {},
+    yAxis: {},
+    series: [{symbolSize: 5, data: scatterData, type: 'scatter', color:'red'}],
+  };
   return (
-    <div>
+    <div className={styles.scatterPlotContainer}>
       <h1>ScatterPlot</h1>
+      <ReactEcharts option={option} />
     </div>
   );
 };
